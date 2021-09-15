6f8bd2ea9109af9243cb425ee9801fc3b6fca27e
Ludmila Botelho <ludmilaasb@gmail.com>
2021-09-15 16:23:34 +0200

    added pithc entropy

--- src/toolbox.py
+++ src/toolbox.py
@@ -1,24 +1,40 @@
 from music21 import converter, corpus, instrument, midi, note, chord, pitch
 import sys
+import numpy as np
 
 # ipt = sys.argv[1]
+
+# for non-numpy array
+# import collections, numpy
+# a = numpy.array([0, 3, 0, 1, 0, 1, 2, 1, 0, 0, 0, 0, 1, 3, 4])
+# collections.Counter(a)
+# Counter({0: 7, 1: 4, 3: 2, 2: 1, 4: 1})
 
 file = converter.parse('stalker.mid')
 # print('This file contains %d tracks'%len(file))
 
 def get_pitches(phrase):
-    notes_list = []
+    pitches_list = []
     for nt in phrase.flat.getElementsByClass(['Note','Chord']):
-        notes_list.append(nt.pitches[-1].ps)
+        pitches_list.append(nt.pitches[-1].ps)
+    return pitches_list
 
-    return notes_list
 def get_rests(phrase):
     rest_list = []
     for nt in phrase.flat.getElementsByClass(['Rest']):
         print(nt.offset)
         rest_list.append(nt.duration.quarterLength/4)
-
     return rest_list
+
+def get_pitch_entropy(phrase):
+    pitches_list = get_pitches(phrase)
+    num_pitches = len(pitches_list_all)
+    unique,counts = np.unique(pitches_list,return_counts=True)
+    dict(zip(unique, counts/num_pitches))
+    pitch_entropy = [-p*np.log(p) for p in counts/num_pitches]
+    return pitch_entropy
+
+
 phrase = file[0].measures(1,7)
 print(get_pitches(phrase))
 print(get_rests(phrase))
