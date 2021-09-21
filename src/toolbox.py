f5761b93f72a869c95c20fd4161f54b61235eb8e
Ludmila Botelho <ludmilaasb@gmail.com>
2021-09-21 15:41:29 +0200

    entropy per track/measure added

--- src/toolbox.py
+++ src/toolbox.py
@@ -1,17 +1,5 @@
 from music21 import converter, corpus, instrument, midi, note, chord, pitch
-import sys
 import numpy as np
-
-# ipt = sys.argv[1]
-
-# for non-numpy array
-# import collections, numpy
-# a = numpy.array([0, 3, 0, 1, 0, 1, 2, 1, 0, 0, 0, 0, 1, 3, 4])
-# collections.Counter(a)
-# Counter({0: 7, 1: 4, 3: 2, 2: 1, 4: 1})
-
-file = converter.parse('stalker.mid')
-# print('This file contains %d tracks'%len(file))
 
 def get_pitches(phrase):
     pitches_list = []
@@ -22,15 +10,13 @@ def get_pitches(phrase):
 def get_rests(phrase):
     rest_list = []
     for nt in phrase.flat.getElementsByClass(['Rest']):
-        print(nt.offset)
         rest_list.append(nt.duration.quarterLength/4)
     return rest_list
 
 def get_rhythm(phrase):
     rhythm_list = []
     count = 0
-    for nt in file[1].measures(1,7).flat.getElementsByClass(['Note','Chord','Rest']):
-        print(nt)
+    for nt in phrase.flat.getElementsByClass(['Note','Chord','Rest']):
         if nt.isRest:
             count+= nt.duration.quarterLength/4
         if nt.isRest == False and count >= 0:
@@ -44,13 +30,18 @@ def get_list_entropy(a_list):
     list_entropy = sum([-p*np.log(p) for p in counts/len(a_list)])
     return list_entropy
 
-
-
-
+def get_entropy_from_measure(file,track,measure):
+    E_p = get_pitches(file.measure(measure).parts[track])
+    E_r = get_rhythm(file.measure(measure).parts[track])
+    E_s = get_rests(file.measure(measure).parts[track])
+    total_entropy = get_list_entropy(E_p) + get_list_entropy(E_r)+ get_list_entropy(E_s)
+    return total_entropy
 
 if __name__ == "__main__":
-    phrase = file[0].measures(1,7)
-    print(get_pitches(phrase))
-    print(get_rests(phrase))
-    print(get_pitch_entropy(phrase))
+    file = converter.parse('stalker.mid')
+
+    phrase = file[1].measures(1,7)
+    print(get_pitches(phrase),get_list_entropy(get_pitches(phrase)))
+    print(get_rests(phrase),get_list_entropy(get_rests(phrase)))
+    print(get_rhythm(phrase),get_list_entropy(get_rhythm(phrase)))
     print(phrase.show('text'))
