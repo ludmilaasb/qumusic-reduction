de4b98ffbfa4893b45fefdd27760d9a7a536ea21
Ludmila Botelho <ludmilaasb@gmail.com>
2021-09-22 14:02:36 +0200

    qubo form implemented

--- src/qubo_form.py
+++ src/qubo_form.py
@@ -4,12 +4,14 @@ from toolbox import *
 def max_num_measures(file):
     return max([len(p) for p in file.parts])
 
-def add_measurecountrains_from(file):
+def qubo_form_from(file):
     H = 0
-    for j in range(50,max_num_measures(file)):
+    for j in range(1,max_num_measures(file)):
         c = 0
+        o = 0
         for i in range(len(file.parts)):
             if file.parts[i].measure(j) != None:
                 c += Binary(f"x_{i}_{j}")
-        H += Constraint((2-c)**2, f"measure_{j}")
-    return H
+                o += get_entropy_from_measure(file,i,j)*Binary(f"x_{i}_{j}")
+        H += Constraint((2-c)**2, f"measure_{j}") + o
+    return H.compile().to_qubo()
