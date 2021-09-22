77b7bf71cb41c24ff1c22dcd52fb7d4f2b1675de
Ludmila Botelho <ludmilaasb@gmail.com>
2021-09-22 15:14:37 +0200

    extracting best track

--- src/qubo_form.py
+++ src/qubo_form.py
@@ -1,10 +1,13 @@
+from dwave.system import EmbeddingComposite, DWaveSampler
+from neal import SimulatedAnnealingSampler
 from pyqubo import Binary, Constraint
 from toolbox import *
+
 
 def max_num_measures(file):
     return max([len(p) for p in file.parts])
 
-def qubo_form_from(file):
+def qubo_form_from_dict(file):
     H = 0
     for j in range(1,max_num_measures(file)):
         c = 0
@@ -15,3 +18,13 @@ def qubo_form_from(file):
                 o += get_entropy_from_measure(file,i,j)*Binary(f"x_{i}_{j}")
         H += Constraint((2-c)**2, f"measure_{j}") + o
     return H.compile().to_qubo()
+
+if __name__ == "__main__":
+
+    file = converter.parse('stalker.mid')
+    Q = qubo_form_from_dict(file)[0]
+    sampler = SimulatedAnnealingSampler()
+    sampleset = sampler.sample_qubo(Q,
+                             num_reads = 10,
+                             label='Example - Get Max Entropy')
+    print(sampleset)
